ab2404de7c702d7471bd7bc29b7db501d21a7178
Candido Hernandez <candido.hern88@gmail.com>
2019-07-24 11:57:02 -0400

    Completed all problems in CH1 - Arrays and Strings

--- 01 - Arrays And Strings/1-7 Rotate_Matrix.py
+++ 01 - Arrays And Strings/1-7 Rotate_Matrix.py
@@ -1,26 +1,52 @@
 import unittest
 
 def rotate_matrix(mat):
-    n = len(mat)
+    """Rotate an n-by-n matrix clockwise 90 degrees
+
+    O(n^2) because no matter what we have to touch every element of the matrix
+
+    Args:
+        mat: An n-by-n matrix
+
+    Returns:
+        mat: matrix rotated by 90 degrees clockwise
+    """
+    n = len(mat)-1
+    for i in range((n+1)//2):
+        for j in range(i,n-i):
+            tmp = mat[i][j]
+            mat[i][j] = mat[n-j][i]
+            mat[n-j][i] = mat[n-i][n-j]
+            mat[n-i][n-j] = mat[j][n-i]
+            mat[j][n-i] = tmp
     return mat
 
 
 class Test(unittest.TestCase):
-    data = [
-        [1,2,3,4],
-        [5,6,7,8],
-        [9,10,11,12],
-        [13,14,15,16]
-    ]
-    answer = [
-        [1,8,12,16],
-        [3,7,11,15],
-        [2,6,10,14],
-        [1,5,9,13]
-    ]
+    data = (
+        (
+            [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]],
+            [[13,9,5,1], [14,10,6,2], [15,11,7,3], [16,12,8,4]]
+        ),
+        (
+            [[1,2,3,4,5,6],
+            [1,2,3,4,5,6],
+            [1,2,3,4,5,6],
+            [1,2,3,4,5,6],
+            [1,2,3,4,5,6],
+            [1,2,3,4,5,6]],
+            [[1,1,1,1,1,1],
+            [2,2,2,2,2,2],
+            [3,3,3,3,3,3],
+            [4,4,4,4,4,4],
+            [5,5,5,5,5,5],
+            [6,6,6,6,6,6]]
+        )
+    )
+
     def test_rotate_matrix(self):
-        output = rotate_matrix(self.data)
-        self.assertEqual(output, self.answer)
+        for case, answer in self.data:
+            self.assertEqual(rotate_matrix(case), answer)
 
 if __name__ == '__main__':
     unittest.main()
--- 01 - Arrays And Strings/1-9 String_Rotation.py
+++ 01 - Arrays And Strings/1-9 String_Rotation.py
@@ -1,11 +1,32 @@
 import unittest
 
-def string_rotation(mat):
-    pass
+def is_substring(string1, string2):
+    return string1 in string2
+
+def string_rotation(string1, string2):
+    """Determine if one string is a rotation of another
+
+    Speed depends on the implementation of the search in is_substring
+
+    Args:
+        string1: string to find
+        string2: possibly rotated string
+
+    Returns:
+        bool: whether string2 is a rotation of string1
+    """
+    return is_substring(string1, string2*2)
+
+    
 
 class Test(unittest.TestCase):
+    data = (
+        (('waterbottle', 'erbottlewat'), True),
+        (('waterbottle', 'notterbottle'), False)
+    )
     def test_string_rotation(self):
-        pass
+        for case, answer in self.data:
+            self.assertEqual(string_rotation(*case), answer)
 
 if __name__ == '__main__':
     unittest.main()
