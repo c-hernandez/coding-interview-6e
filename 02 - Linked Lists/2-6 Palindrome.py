0b8d05d20ab8165ae6b09a2a05371c797648f115
Candido Hernandez <candido.hern88@gmail.com>
2019-07-26 22:44:28 -0400

    Completed problem 2-6

--- 02 - Linked Lists/2-6 Palindrome.py
+++ 02 - Linked Lists/2-6 Palindrome.py
@@ -2,13 +2,46 @@ from SLL import SLL
 import unittest
 
 
-def palindrome(sll, pivot):
-    pass
+def palindrome(sll):
+    fast = sll.head
+    slow = sll.head
+
+    # Using a list a a stack here for simplicity.
+    # I'll leave a custom implementation for the next chapter
+    stack = []
+    while fast and fast.next:
+        stack.append(slow.data)
+        slow = slow.next
+        fast = fast.next.next
+
+    # This will skip the middle of odd sized lists
+    if fast is not None:
+        slow = slow.next
+    
+    # Now check for equality of the left and right halves
+    while slow:
+        if slow.data != stack.pop():
+            return False
+        slow = slow.next
+    return True
+
 
 
 class Test(unittest.TestCase):
+
     def test_palindrome(self):
-        pass
+        cases = (
+            (SLL(data=list('abcdefgfedcba')), True),
+            (SLL(data=list('fasttsaf')), True),
+            (SLL(data='amanaplanacanalpanama'), True),
+            (SLL(data=list('notapalindrome')), False),
+            (SLL(data=list('notapalindrome!')), False)
+        )
+
+        for case, answer in cases:
+            print(case)
+            self.assertEqual(palindrome(case), answer)
+
 
 
 if __name__ == '__main__':
